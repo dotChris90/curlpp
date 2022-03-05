90438f935d3854338f795bcd8742d4952fa1c720
dotChris90 <Christian.Kahr@gmx.de>
2022-03-05 11:27:23 -0500

    Added conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,54 @@
+
+from json import tool
+from conans import ConanFile
+from conans import tools
+from conan.tools.cmake import CMakeToolchain, CMake, CMakeDeps
+from conan.tools.layout import cmake_layout
+
+
+class CurlppConan(ConanFile):
+
+    name = "curlpp"
+    version = "0.8.1"
+    # Optional metadata
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Abc here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "src/*", "doc/*", "examples/*", "extras/*", "include/*"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+    
+    def requirements(self):
+        self.requires("libcurl/7.64.1")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+        cmake = CMakeDeps(self)
+        cmake.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
