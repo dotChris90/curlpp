1b53bfc77419381f0cfbdbb8b77e06edc6b65336
dotChris90 <Christian.Kahr@gmx.de>
2022-03-07 04:18:21 -0500

    Bug fix : cmake/* was missing

--- conanfile.py
+++ conanfile.py
@@ -23,7 +23,7 @@ class CurlppConan(ConanFile):
     default_options = {"shared": False, "fPIC": True}
 
     # Sources are located in the same place as this recipe, copy them to the recipe
-    exports_sources = "CMakeLists.txt", "src/*", "doc/*", "examples/*", "extras/*", "include/*"
+    exports_sources = "CMakeLists.txt", "src/*", "doc/*", "examples/*", "extras/*", "include/*", "cmake/*"
 
     def config_options(self):
         if self.settings.os == "Windows":
